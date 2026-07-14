c832456aeb205d87213f63be98e5d83a35054eef
Manuel Esquivel <mesquivelbriones836@gmail.com>
2026-07-14 16:39:57 -0600

    Primer commit

---
+++ models/staging/staging_tiie.sql
@@ -0,0 +1,9 @@
+SELECT
+cast(fecha as date)as fecha,
+cast(nullif(TIIE_a_182_dias, "N/E")as numeric) as TIIE_a_182_dias,
+cast(nullif(tiie_dia_habil_bancario, "N/E")as numeric)as tiie_dia_habil_bancario,
+cast(nullif(Tasa_de_fondeo_bancario_anual, "N/E")as numeric) as Tasa_de_fondeo_bancario_anual,
+cast(nullif(tasa_de_fondeo_gubernamental_anual, "N/E")as numeric ) as tasa_de_fondeo_gubernamental_anual,
+cast(nullif(tiie_a_28_dias, "N/E")as numeric)as tiie_a_28_dias,
+cast(nullif(Tasa_objetivo, "N/E")as numeric)as Tasa_objetivo
+from {{ source('banxico', 'TIIE_08_26') }}
